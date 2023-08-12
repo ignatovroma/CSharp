41a478060ee8f3180c39f0e603379d071d6ec156
IgnatovRoman <oneok@bk.ru>
2023-08-12 23:13:36 +0300

    string variant

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -6,33 +6,33 @@
 
 
 Console.WriteLine("Введите номер от 01 до 07 номер недели");
-int num = Convert.ToInt32(Console.ReadLine());
+string num = Console.ReadLine();
 
-    if (num == 1)
+    if (num == "1")
     {
         Console.WriteLine("Понедельник");
     }
-    else if (num == 2) 
+    else if (num == "2") 
     {
         Console.WriteLine("Вторник");
     }
-    else if (num == 3) 
+    else if (num == "3") 
     {
         Console.WriteLine("Среда");
     }
-    else if (num == 4) 
+    else if (num == "4") 
     {
         Console.WriteLine("Четверг");
     }
-    else if (num == 5) 
+    else if (num == "5") 
     {
         Console.WriteLine("Пятница");
     }
-    else if (num == 6) 
+    else if (num == "6") 
     {
         Console.WriteLine("Суббота");
     }
-    else if (num == 7) 
+    else if (num == "7") 
     {
         Console.WriteLine("Воскресенье");
     }
