7c3e62a9c5339b52ce1d18c6c735b607ceb83e45
IgnatovRoman <oneok@bk.ru>
2023-09-03 23:45:17 +0300

    seminar

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -3,26 +3,26 @@
 // нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-// void FillArray(int[] array)
-// {
-// Random rnd = new Random();
+void FillArray(int[] array)
+{
+    Random rnd = new Random();
 
-// for (int i = 0; i < array.Length; i++)
-// {
-// array[i] = rnd.Next(2);
-// }
-// }
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(2);
+    }
+}
 
-// void PrintArray(int[] array)
-// {
-// for (int i = 0; i < array.Length; i++)
-// {
-// Console.Write(array[i] + " ");
-// }
-// }
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
 
-// int[] arr = new int[8];
+int[] arr = new int[8];
 
-// FillArray(arr);
-// PrintArray(arr);
+FillArray(arr);
+PrintArray(arr);
 
