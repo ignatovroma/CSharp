c512408b3ab3b674ebf9c3f1d378b58750329d74
IgnatovRoman <oneok@bk.ru>
2023-09-04 22:28:54 +0300

    seminar

---
+++ Task40/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник со сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+bool IsExistTriangle(int side1, int side2, int side3)
+{
+return side1 < side2 + side3 && side2 < side1 + side3 && side3 < side2 + side1;
+}
+
+Console.WriteLine("Введите первую сторону:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второую сторону:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третью сторону:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsExistTriangle (num1, num2, num3);
+Console.WriteLine(result? "Треугольник существует" : "Треугольник не существует");
+
---
+++ Task44/Program.cs
@@ -0,0 +1,35 @@
+﻿// Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. 
+// Первые два числа Фибоначчи: 0 и 1.
+
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int[] FiboArray(int num)
+{
+    int[] array = new int[num];
+    // array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < num; i++)
+    {
+        array[i] = array[i - 2] + array[i - 1];
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+Console.WriteLine("Введите число больше 2:");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] result = FiboArray(number);
+PrintArray(result);
