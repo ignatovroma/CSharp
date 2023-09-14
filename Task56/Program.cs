546088d54cef6a0a77b7e8fcf9b54dd7b6c2cfc4
IgnatovRoman <oneok@bk.ru>
2023-09-14 18:17:16 +0300

    dz 54 56 58 60 62

---
+++ Task56/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить 
+// строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке 
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateArrayRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int[] SumsNumsRows(int[,] matrix)
+{
+    int[] arrSumsRows = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sumRows = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumRows += matrix[i, j];
+        }
+        arrSumsRows[i] = sumRows;
+        Console.Write($" {sumRows:D2}"+"; ");
+        Console.WriteLine();
+    }
+    return arrSumsRows; 
+}
+
+int MinSumNumRow(int[] arr)
+{
+    int minsum = arr[0];
+    int minsumindex = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (minsum > arr[i]) 
+        {
+            minsum = arr[i]; 
+            minsumindex = i;
+        }    
+    }
+    return minsumindex + 1;
+}
+
+int[,] array = CreateArrayRndInt(4, 4, -9, 9);
+PrintMatrix(array);
+int[] sumsNumsRows = SumsNumsRows(array);
+int res = MinSumNumRow(sumsNumsRows);
+Console.WriteLine($"Строка с наименьшей суммой элементов -> {res} строка");
---
+++ Task62/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+
+//     -0- -1- -2- -3-
+// -0-  01  02  03  04
+// -1-  12  13  14  05
+// -2-  11  16  15  06
+// -3-  10  09  08  07
+
+int Promt(string message)
+{
+    Console.Clear();
+    Console.Write(message); //вывести сообщение
+    var value = Console.ReadLine(); //считывает с консоли строку
+    int result = Convert.ToInt32(value); //преобразует строку в целое число
+    Console.Clear();
+    return result;
+}
+
+int[,] CreateSpiralMatrix(int sizeSideMatrix)
+{
+    int[,] matrix = new int[sizeSideMatrix, sizeSideMatrix];
+
+    int firstRow = 0,
+        lastRow = sizeSideMatrix - 1,
+        firstColumn = 0,
+        lastColumn = sizeSideMatrix - 1;
+
+    int value = 1;
+
+    while (value <= sizeSideMatrix * sizeSideMatrix)
+    {
+        for (int i = firstColumn; i <= lastColumn; i++)
+            matrix[firstRow, i] = value++;
+        firstRow++;
+
+        for (int i = firstRow; i <= lastRow; i++)
+            matrix[i, lastColumn] = value++;
+        lastColumn--;
+
+        for (int i = lastColumn; i >= firstColumn; i--)
+            matrix[lastRow, i] = value++;
+        lastRow--;
+
+        for (int i = lastRow; i >= firstRow; i--)
+            matrix[i, firstColumn] = value++;
+        firstColumn++;
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]:D2} ");
+        }
+        Console.WriteLine("|");
+    }
+    Console.WriteLine();
+    Console.ReadLine();
+}
+
+int number = Promt("Введите длину стороны матрицы -> ");
+var result = CreateSpiralMatrix(number);
+PrintMatrix(result);
